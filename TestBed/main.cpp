2673550f1372cfa3743f0acc02f604f6821979d7
Trent_T <trenttuedon2000@gmail.com>
2026-05-09 17:12:23 -0400

    make square move up

--- TestBed/main.cpp
+++ TestBed/main.cpp
@@ -1,4 +1,9 @@
 #include <Engine.h>
+#include <string>
+#include "Entity.h"
+#include "Event.h"
+#include "EventKeyboard.h"
+#include "Logger.h"
 void TestLogger()
 {
 	ENGINE.getLogger()->log(HS::ERROR, "My Error"); 
@@ -10,10 +15,51 @@ void TestLogger()
 
 void TestRenderer()
 {
+	class Square: public HS::Entity
+	{
+		public:
+			
+			Square()
+			{
+				this->setPosition(HS::Vector{100, 100}); 
+			}
+
+			~Square(){}; 
+
+			void draw()
+			{
+				ENGINE.getRenderer()->drawRectangle(this->getPosition().getX(), this->getPosition().getY(), 100, 100); 
+			}
+
+			void update(const HS::Event* e)
+			{
+				if (e->getType() == HS::KEYBOARD_EVENT) 
+				{
+					const HS::EventKeyboard* keyEvent = dynamic_cast<const HS::EventKeyboard*>(e);
+
+					if (keyEvent->getKey() == HS::KeyboardKey::W) 
+					{
+						ENGINE.getLogger()->log(HS::INFO, "HERE"); 
+						HS::Vector up{0, -1};
+
+						HS::Vector new_position = up + this->getPosition(); 
+
+						this->setPosition(new_position);
+
+						ENGINE.getLogger()->log(HS::INFO, std::to_string(new_position.getY()));
+					}
+				}
+			}
+	}; 
+
+	Square* square{new Square};
+
+	ENGINE.getWorld()->addEntity(square); 
+
+	ENGINE.Run();
 }
 
 int main()
 {
-	TestLogger(); 
-
+	TestRenderer();
 }
